83f753dc25d198ea52cd1a8c8e3ff46d8245260a
Yamini Singh <jasawatyamini1311@gmail.com>
2026-08-26 21:53:55 +0530

    Add while loop practice

---
+++ loops.js
@@ -0,0 +1,14 @@
+// JavaScript Loops Practice
+
+// 1. Create a starting value
+let i = 1;
+
+// 2. Write the while condition
+while (i <= 10) {
+
+    // 3. Code that should repeat
+    console.log(i);
+
+    // 4. Change the value
+    i++;
+}
