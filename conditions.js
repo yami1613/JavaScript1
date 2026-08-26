b8aa6c47d968ade81323123825876f368df9ae71
Yamini Singh <jasawatyamini1311@gmail.com>
2026-08-26 21:47:24 +0530

    Add condition practice

---
+++ conditions.js
@@ -0,0 +1,18 @@
+// JavaScript Conditions Practice
+
+let age = 18;
+
+if (age >= 18) {
+    console.log("Adult");
+} else {
+    console.log("Minor");
+}
+
+
+let marks = 55;
+
+if(marks >= 50) {
+  console.log("Pass");
+} else {
+  console.log("Fail");
+}
