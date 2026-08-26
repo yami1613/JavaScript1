7681e38d43094f4be8858c59125542b77470ee08
Yamini Singh <jasawatyamini1311@gmail.com>
2026-08-26 21:40:06 +0530

    Add JavaScript basics

---
+++ basics.js
@@ -0,0 +1,33 @@
+// JavaScript Basics
+
+// Variables
+let name = "Yamini";
+let age = 18;
+let course = "AI & ML";
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Course:", course);
+
+// Array
+let skills = ["HTML", "CSS", "JavaScript"];
+
+console.log("Skills:", skills);
+
+// Conditional Statement
+if (age >= 18) {
+    console.log("You are eligible to vote.");
+} else {
+    console.log("You are not eligible to vote.");
+}
+
+// Function
+function greet(name) {
+    if (name === "Yamini") {
+        console.log("Hello, Yamini!");
+    } else {
+        console.log("Hello, stranger!");
+    }
+}
+
+greet(name);
