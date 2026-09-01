ab05472deb844f9d9eeb1f9e2ffbf68f3bf344d4
Yamini Singh <jasawatyamini1311@gmail.com>
2026-09-01 20:31:43 +0530

    Practice map method
    Added a map example to transform an array of numbers into their squares.

--- arrays.js
+++ arrays.js
@@ -52,3 +52,13 @@ if (index !== -1) {
 } else {
     console.log("Banana is not found");
 }
+
+// Map Practice
+
+let numbers = [1, 2, 3, 4, 5];
+// Transform each element using map()
+let squares = numbers.map(function(number) {
+    return number * number;
+});
+
+console.log(squares);
